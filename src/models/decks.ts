0b44a1539d7ddb24b1e335860438636218f9e883
Wagenheim <wagenheimk@me.com>
2020-04-22 22:06:08 -0400

    added deck constructor logic

---
+++ src/models/decks.ts
@@ -0,0 +1,77 @@
+export class Deck{
+
+    cardOne: number;
+    cardTwo: number;
+    cardThree: number;
+    cardFour: number;
+    cardFive: number;
+    cardSix: number;
+    cardSeven: number;
+    cardEight: number;
+    cardNine: number;
+    cardTen: number;
+    cardEleven: number;
+    cardTwelve: number;
+    cardThirteen: number;
+    cardFourteen: number;
+    cardFifteen: number;
+    cardSixteen: number;
+    cardSeventeen: number;
+    cardEighteen: number;
+    cardNineteen: number;
+    cardTwenty: number;
+    cardTwentyOne:number;
+    cardTwentyTwo: number;
+    cardTwentyThree: number;
+    cardTwentyFour: number;
+    cardTwentyFive: number;
+    cardTwentySix: number;
+    cardTwentySeven: number;
+    cardTwentyEight: number;
+    cardTwentyNine: number;
+    cardThirty: number;
+    name: string;
+    description: string;
+
+    constructor(name:string, desc: string, cOne:number, cTwo: number, cThree: number, cFour: number, cFive: number, cSix: number, 
+                cSeven: number, cEight: number, cNine: number, cTen: number, cEleven: number, cTwelve: number, cThirteen: number, 
+                cFourteen: number, cFifteen: number, cSixteen: number, cSeventeen: number, cEighteen: number, cNineteen: number, 
+                cTwenty: number, cTwentyOne: number, cTwentyTwo: number, cTwentyThree: number, cTwentyFour: number, cTwentyFive: number, 
+                cTwentySix: number, cTwentySeven: number, cTwentyEight: number, cTwentyNine: number, cThirty: number){
+
+                    this.name = name;
+                    this.description = desc;
+                    this.cardOne = cOne;
+                    this.cardTwo = cTwo;
+                    this.cardThree = cThree;
+                    this.cardFour = cFour;
+                    this.cardFive = cFive;
+                    this.cardSix = cSix;
+                    this.cardSeven = cSeven;
+                    this.cardEight = cEight;
+                    this.cardNine = cNine;
+                    this.cardTen = cTen;
+                    this.cardEleven = cEleven;
+                    this.cardTwelve = cTwelve;
+                    this.cardThirteen = cThirteen;
+                    this.cardFourteen = cFourteen;
+                    this.cardFifteen = cFifteen;
+                    this.cardSixteen = cSixteen;
+                    this.cardSeventeen = cSeventeen;
+                    this.cardEighteen = cEighteen;
+                    this.cardNineteen = cNineteen;
+                    this.cardTwenty = cTwenty;
+                    this.cardTwentyOne = cTwentyOne;
+                    this.cardTwentyTwo = cTwentyTwo;
+                    this.cardTwentyThree = cTwentyThree;
+                    this.cardTwentyFour = cTwentyFour;
+                    this.cardTwentyFive = cTwentyFive;
+                    this.cardTwentySix = cTwentySix;
+                    this.cardTwentySeven = cTwentySeven;
+                    this.cardTwentyEight = cTwentyEight;
+                    this.cardTwentyNine = cTwentyNine;
+                    this.cardThirty = cThirty;
+
+                }
+
+} 
