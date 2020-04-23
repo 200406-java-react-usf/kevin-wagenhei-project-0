144d3777ac07056ef257840bb9ee9d0e28942ddd
Wagenheim <wagenheimk@me.com>
2020-04-22 21:30:11 -0400

    added crud repo interface

---
+++ src/repos/crud-repo.ts
@@ -0,0 +1,9 @@
+export interface CrudRepository<T> {
+
+    getAll(): Promise<T>;
+    getById(): Promise<T>;
+    save(newObj: T): Promise<T>;
+    update(updatedObj: T): Promise<boolean>;
+    deleteById(id: number): Promise<boolean>;
+
+}
