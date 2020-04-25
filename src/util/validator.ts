9a30da6dd858a09f0b98caef335c7f91652c75a5
Wagenheim <wagenheimk@me.com>
2020-04-25 17:29:31 -0400

    added an isvalidobject check to validator

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -8,3 +8,17 @@ export const isValidId = (id: number): boolean => {
 
 }
 
+export const isValidObject = (obj: Object, ...nullableVal: string[]) => {
+
+    return obj && Object.keys(obj).every(key => {
+
+        if (nullableVal.includes(key)){
+            return true;
+        }
+
+        return obj[key];
+
+    });
+
+}
+
