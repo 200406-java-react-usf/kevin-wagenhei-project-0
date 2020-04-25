d4858265dd47fba9510008bfd442406aaf0fcf06
Wagenheim <wagenheimk@me.com>
2020-04-25 15:22:41 -0400

    fixed issue where getAll needed to return a promise of type T[] not type T

--- src/repos/crud-repo.ts
+++ src/repos/crud-repo.ts
@@ -1,6 +1,6 @@
 export interface CrudRepository<T> {
 
-    getAll(): Promise<T>;
+    getAll(): Promise<T[]>;
     getById(): Promise<T>;
     save(newObj: T): Promise<T>;
     update(updatedObj: T): Promise<boolean>;
