61bac66c6fe37339009acc4719eeac23e5185105
Wagenheim <wagenheimk@me.com>
2020-04-25 21:34:45 -0400

    added a deck ID, author ID, and a deck name to the constructor of the deck obj

--- src/models/decks.ts
+++ src/models/decks.ts
@@ -2,10 +2,16 @@ import {Card} from './cards';
 
 export class Deck{
 
+    deckId: number;
+    authorId: number
+    deckname: string;
     deckArray: Card[];
 
-    constructor(da: Card[]){
+    constructor(id:number, authorId: number, name:string, da: Card[]){
 
+        this.deckId = id;
+        this.authorId = authorId;
+        this.deckname = name;
         this.deckArray = da;
 
     }
