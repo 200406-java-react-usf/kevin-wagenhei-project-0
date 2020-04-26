f259586aca65fd6f05b0e4ea40c0c59c34721d62
Wagenheim <wagenheimk@me.com>
2020-04-25 21:45:31 -0400

    added two decks to the mock DB

---
+++ src/data/deck-db.ts
@@ -0,0 +1,81 @@
+import {Deck} from '../models/decks';
+import {Card} from '../models/cards';
+
+let id: number = 1;
+
+let deckOneArray: Card[] = [
+
+    new Card(id++, 'Escrivate', 'Common', 12.3, 1.4, 57.1, 100000, 54),
+    new Card(id++, 'Eaglehorn Bow', 'Rare', 5.1, 1.9, 57.2, 39000, 53.6),
+    new Card(id++, 'Shadow Madness', 'Rare', 3.8, 1.2, 53.4, 30000, 52),
+    new Card(id++, 'Mass Dispell', 'Rare', 3.5, 1.2, 50.5, 28000, 50.4),
+    new Card(id++, 'Loot Hoarder', 'Common', 2.4, 1.8, 46.5, 26000, 42.4),
+    new Card(id++, 'Power of the Wild', 'Common', 3.7, 1.9, 53.5, 47000, 55.6),
+    new Card(id++, 'Far Sight', 'Epic', 2.1, 1.5, 51.5, 24000, 46.3),
+    new Card(id++, 'Doomsayer', 'Epic', 6.5, 1, 55.3, 36000, 45.6),
+    new Card(id++, 'Brightwing', 'Legendary', 1.3, 1, 48.5, 24000, 50.2),
+    new Card(id++, 'Edwin Vancleef', 'Legendary', 11.6, 1, 57.9, 63000, 60.8),
+    new Card(id++, 'Murloc Warleader', 'Epic', 2.9, 2, 58.2, 23000, 56.1),
+    new Card(id++, 'Leper Gnome', 'Common', 4.5, 1.9, 57.8, 41000, 52.8),
+    new Card(id++, 'Coldlight Seer', 'Rare', 3, 2, 58, 23000, 53.6),
+    new Card(id++, 'Alexstraza', 'Legendary', 13.2, 1, 54.4, 49000, 62.6),
+    new Card(id++, 'Ice Barriar', 'Common', 9.7, 1.2, 54, 95000, 49),
+    new Card(id++, 'Escrivate', 'Common', 12.3, 1.4, 57.1, 100000, 54),
+    new Card(id++, 'Eaglehorn Bow', 'Rare', 5.1, 1.9, 57.2, 39000, 53.6),
+    new Card(id++, 'Shadow Madness', 'Rare', 3.8, 1.2, 53.4, 30000, 52),
+    new Card(id++, 'Mass Dispell', 'Rare', 3.5, 1.2, 50.5, 28000, 50.4),
+    new Card(id++, 'Loot Hoarder', 'Common', 2.4, 1.8, 46.5, 26000, 42.4),
+    new Card(id++, 'Power of the Wild', 'Common', 3.7, 1.9, 53.5, 47000, 55.6),
+    new Card(id++, 'Far Sight', 'Epic', 2.1, 1.5, 51.5, 24000, 46.3),
+    new Card(id++, 'Doomsayer', 'Epic', 6.5, 1, 55.3, 36000, 45.6),
+    new Card(id++, 'Brightwing', 'Legendary', 1.3, 1, 48.5, 24000, 50.2),
+    new Card(id++, 'Edwin Vancleef', 'Legendary', 11.6, 1, 57.9, 63000, 60.8),
+    new Card(id++, 'Murloc Warleader', 'Epic', 2.9, 2, 58.2, 23000, 56.1),
+    new Card(id++, 'Leper Gnome', 'Common', 4.5, 1.9, 57.8, 41000, 52.8),
+    new Card(id++, 'Coldlight Seer', 'Rare', 3, 2, 58, 23000, 53.6),
+    new Card(id++, 'Alexstraza', 'Legendary', 13.2, 1, 54.4, 49000, 62.6),
+    new Card(id++, 'Ice Barriar', 'Common', 9.7, 1.2, 54, 95000, 49),
+
+] 
+
+let deckTwoArray: Card[] = [
+
+    new Card(id++, 'Escrivate', 'Common', 12.3, 1.4, 57.1, 100000, 54),
+    new Card(id++, 'Eaglehorn Bow', 'Rare', 5.1, 1.9, 57.2, 39000, 53.6),
+    new Card(id++, 'Shadow Madness', 'Rare', 3.8, 1.2, 53.4, 30000, 52),
+    new Card(id++, 'Mass Dispell', 'Rare', 3.5, 1.2, 50.5, 28000, 50.4),
+    new Card(id++, 'Loot Hoarder', 'Common', 2.4, 1.8, 46.5, 26000, 42.4),
+    new Card(id++, 'Power of the Wild', 'Common', 3.7, 1.9, 53.5, 47000, 55.6),
+    new Card(id++, 'Far Sight', 'Epic', 2.1, 1.5, 51.5, 24000, 46.3),
+    new Card(id++, 'Doomsayer', 'Epic', 6.5, 1, 55.3, 36000, 45.6),
+    new Card(id++, 'Brightwing', 'Legendary', 1.3, 1, 48.5, 24000, 50.2),
+    new Card(id++, 'Edwin Vancleef', 'Legendary', 11.6, 1, 57.9, 63000, 60.8),
+    new Card(id++, 'Murloc Warleader', 'Epic', 2.9, 2, 58.2, 23000, 56.1),
+    new Card(id++, 'Leper Gnome', 'Common', 4.5, 1.9, 57.8, 41000, 52.8),
+    new Card(id++, 'Coldlight Seer', 'Rare', 3, 2, 58, 23000, 53.6),
+    new Card(id++, 'Alexstraza', 'Legendary', 13.2, 1, 54.4, 49000, 62.6),
+    new Card(id++, 'Ice Barriar', 'Common', 9.7, 1.2, 54, 95000, 49),
+    new Card(id++, 'Escrivate', 'Common', 12.3, 1.4, 57.1, 100000, 54),
+    new Card(id++, 'Counterspell', 'Rare', 1.7, 1.4, 50.3, 29000, 55.9),
+    new Card(id++, 'Shadow Madness', 'Rare', 3.8, 1.2, 53.4, 30000, 52),
+    new Card(id++, 'Mass Dispell', 'Rare', 3.5, 1.2, 50.5, 28000, 50.4),
+    new Card(id++, 'Loot Hoarder', 'Common', 2.4, 1.8, 46.5, 26000, 42.4),
+    new Card(id++, 'Mirror Entity', 'Common', 1, 1.4, 46.9, 23000, 50.8),
+    new Card(id++, 'Far Sight', 'Epic', 2.1, 1.5, 51.5, 24000, 46.3),
+    new Card(id++, 'Explosive Trap', 'Common', 7.3, 1.8, 56.4, 50000, 46.2),
+    new Card(id++, 'Brightwing', 'Legendary', 1.3, 1, 48.5, 24000, 50.2),
+    new Card(id++, 'Edwin Vancleef', 'Legendary', 11.6, 1, 57.9, 63000, 60.8),
+    new Card(id++, 'Murloc Warleader', 'Epic', 2.9, 2, 58.2, 23000, 56.1),
+    new Card(id++, 'Shadow Word: Ruin', 'Epic', 4.6, 1.1, 53.4, 23000, 50.4),
+    new Card(id++, 'Coldlight Seer', 'Rare', 3, 2, 58, 23000, 53.6),
+    new Card(id++, 'Alexstraza', 'Legendary', 13.2, 1, 54.4, 49000, 62.6),
+    new Card(id++, 'Ice Barriar', 'Common', 9.7, 1.2, 54, 95000, 49),
+
+] 
+
+export default [
+
+    new Deck(id++, 1, 'Kevin\'s Deck', deckOneArray),
+    new Deck(id++, 3, 'Nick\'s Deck', deckTwoArray)
+
+]
