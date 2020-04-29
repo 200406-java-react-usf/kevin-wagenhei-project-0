ae6dda5e200b3ac6730dcf77dbd2dd5cc35808ec
Wagenheim <wagenheimk@me.com>
2020-04-29 10:57:15 -0400

    added deck service to config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,15 @@
 import {CardRepository} from '../repos/card-repo';
 import {CardService} from '../services/card-services';
+import {DeckRepository} from '../repos/deck-repo';
+import {DeckService} from '../services/deck-services';
 
 const cardRepo = new CardRepository();
 const cardService = new CardService(cardRepo);
 
+const deckRepo = new DeckRepository();
+const deckService = new DeckService(deckRepo);
+
 export default {
-    cardService
+    cardService,
+    deckService
 };
