b3857911c62175440c503c72cdeabf64b8a56980
Wagenheim <wagenheimk@me.com>
2020-04-29 22:47:05 -0400

    implemented routers for each users,decks, and cards

---
+++ src/routers/card-router.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import AppConfig from '../config/app';
+
+export const CardRouter = express.Router();
+
+let cardService = AppConfig.cardService;
+
+CardRouter.get('', async(req,resp) => {
+
+    try{
+
+        let payload = await cardService.getAllCards();
+        resp.status(200).json(payload);
+
+    } catch (e){
+        resp.status(e.statusCode).json(e);
+    }
+
+    resp.send();
+
+});
+
+CardRouter.get('/:id', async (req, resp) => {
+
+    const id = +req.params.id;
+
+    try{
+        let payload = await cardService.getCardById(id);
+        resp.status(200).json(payload);
+    } catch(e){
+        resp.status(e.statusCode).json(e);
+    }
+
+    resp.send();
+
+});
