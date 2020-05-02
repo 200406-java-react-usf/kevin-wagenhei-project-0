06916bb946f5254e7fff4dd1a27bcb325bb3c462
Wagenheim <wagenheimk@me.com>
2020-05-02 14:27:28 -0400

    updated the user model and TS db and separed first and last names

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -4,10 +4,10 @@ let id = 1;
 
 export default [
 
-    new User(id++, 'Wagenheim', 'Kevin Wagenheim', 'wagenheimk@me.com', 'password'),
-    new User(id++, 'CameraGuyJohn', 'John Egan', 'jvemedia@gmail.com', 'password'),
-    new User(id++, 'Vacseal', 'Nick Aliantro', 'naliantro@live.com', 'password'),
-    new User(id++, 'LzM', 'Louis Monacello', 'LzM_@me.com', 'password'),
-    new User(id++, 'MuffErickson', 'Tyler Erickson', 'MuffErickson@gmail.com', 'password')
+    new User(id++, 'Wagenheim', 'Kevin', 'Wagenheim', 'wagenheimk@me.com', 'password'),
+    new User(id++, 'CameraGuyJohn', 'John', 'Egan', 'jvemedia@gmail.com', 'password'),
+    new User(id++, 'Vacseal', 'Nick', 'Aliantro', 'naliantro@live.com', 'password'),
+    new User(id++, 'LzM', 'Louis', 'Monacello', 'LzM_@me.com', 'password'),
+    new User(id++, 'MuffErickson', 'Tyler', 'Erickson', 'MuffErickson@gmail.com', 'password')
 
 ];
