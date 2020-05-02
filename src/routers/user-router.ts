d36038737466af2718330ffdf1944527e3919a20
Wagenheim <wagenheimk@me.com>
2020-05-02 14:53:03 -0400

    implemented logic for post req adding a user

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppConfig from '../config/app';
+import e from 'express';
 
 export const UserRouter = express.Router();
 
@@ -25,6 +26,19 @@ UserRouter.get('/:id', async(req, resp) => {
     try{
         let payload = await userService.getUserById(id);
         resp.status(200).json(payload);
+    } catch (e){
+        resp.status(e.statusCode).json(e);
+    }
+
+    resp.send();
+
+});
+
+UserRouter.post('', async(req, resp) => {
+
+    try{
+        let payload = await userService.addNewUser(req.body);
+        resp.status(201).json(payload);
     } catch (e){
         resp.status(e.statusCode).json(e);
     }
