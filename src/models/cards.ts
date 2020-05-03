10d88e47b08de9bb8206c19af2dce38b77644f7f
Wagenheim <wagenheimk@me.com>
2020-05-02 21:46:22 -0400

    had inconsistent naming, changed the names in card model to be consistent with JSON obj

--- src/models/cards.ts
+++ src/models/cards.ts
@@ -3,16 +3,16 @@ export class Card {
     id: number;
     name: string;
     rarity: string;
-    deckWinRate: number; //win rate of the decks this card is in
-    playedWinRate: number; //win rate of the decks when this card has been played
+    deckWinrate: number; //win rate of the decks this card is in
+    playedWinrate: number; //win rate of the decks when this card has been played
 
     constructor(id: number, name: string, rarity: string, dwr: number, pwr: number){
 
         this.id = id;
         this.name = name;
         this.rarity = rarity;
-        this.deckWinRate = dwr;
-        this.playedWinRate = pwr;
+        this.deckWinrate = dwr;
+        this.playedWinrate = pwr;
 
     }
 
